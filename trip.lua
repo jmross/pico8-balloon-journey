3da1f21263d914f4fb95ac4ee4276cd2378b8bc1
Jesse Ross <jesseross92@gmail.com>
2024-04-30 11:59:16 -0400

    start and end game working. scrolling stars. take damage. show score and lives, water

--- trip.lua
+++ trip.lua
@@ -4,12 +4,18 @@ min_y = 0
 max_x = 127
 max_y = 127
 
-grav = 0.25
+grav = 0
 x_resist = 0.98
+scroll_speed = -1
+started = false
+ended = false
+score = 0
 
 -- player
 p = {
-  size = 3,
+  lives = 3,
+
+  size = 4,
   x = 64,
   y = 64,
   dx = 0,
@@ -25,8 +31,15 @@ p = {
   max_grav_speed = 3
 }
 
+function p:damage()
+  self.lives -= 1
+  if(self.lives <= 0) then
+    lose()
+  end
+end
+
 function p:draw()
-  circfill(self.x, self.y, self.size, 14)
+  circfill(self.x, self.y, self.size, 7)
 end
 
 function p:move_left()
@@ -67,14 +80,11 @@ function p:update()
 
   -- bounce off ceiling
   if(self.y < self.size) then self.dy = 0.5*abs(self.dy) self.y = self.size end
-  -- lose if touch floor
+
+  -- damage if touch floor
   if(self.y > max_y - self.size) then
-    printh("YOU LOSE")
     self.y = max_y - self.size
-    self.dx = 0
-    self.dy = 0
-    self.ddx = 0
-    self.ddy = 0
+    self:damage()
   end
 
   -- stop on left/right walls
@@ -86,7 +96,7 @@ end
 enemies = {}
 -- enemy
 enemy = {
-  size = 1,
+  size = 2,
   x = 0,
   y = 0,
   dx = 0,
@@ -101,47 +111,166 @@ function enemy:update()
   -- bounce off floor
   if(self.y > max_y - self.size) then self.dy = -abs(self.dy) self.y = max_y - self.size end
 
-  -- stop on left/right walls
-  if(self.x < self.size) then self.dx = abs(self.dx) self.x = self.size end
-  if(self.x > max_x - self.size) then self.dx = -abs(self.dx) self.x = max_x - self.size end
+  -- re-init on touch of walls
+  if(self.x < -self.size) then 
+    self:init()
+  end
+
+  if(self:collide(p)) then
+    p:damage()
+    self:init()
+  end
 end
+
 function enemy:draw()
   circfill(self.x, self.y, self.size, 14)
 end
+
 function enemy:new(o)
   return setmetatable(o or {}, self)
+end
+
+function enemy:collide(o)
+  return self.x >= (o.x - o.size) and self.x <= (o.x + o.size) and self.y >= (o.y - o.size) and self.y <= (o.y + o.size)
+end
+
+function enemy:init()
+  self.x = max_x + 1
+  self.y = rnd(max_y)
+  self.dx = -rnd(2)
+  -- TODO
+  self.dy = rnd(1) + (flr(rnd(2)) * -1)
+end
+
+-- platforms
+platforms = {}
+-- starting platform
+platform = { 
+  x = 0,
+  y = 0,
+  width = 10,
+  height = 3,
+  colour = 14,
+  dx = scroll_speed,
+  dy = 0,
+}
+platform.__index = platform
+function platform:new(o)
+  return setmetatable(o or {}, self)
+end
+function platform:draw()
+  rectfill(self.x, self.y, self.x + self.width, self.y + self.height, self.colour)
+end
+
+-- starting platform
+start_platform = platform:new({
+    x = max_x / 2 - 4,
+    y = max_y / 2 + 5
+  })
+add(platforms, start_platform)
+
+-- stars
+stars ={}
+-- star
+star = {
+  x = 0,
+  y = 0,
+  size = 0,
+  colour = 10,
+}
+star.__index = star
+function star:new(o)
+  return setmetatable(o or {}, self)
+end
+function star:update()
+  self.x += scroll_speed
+  -- move to other side on touch of walls
+  if(self.x < -self.size) then 
+    self.x = max_x + self.size
+    self.y = rnd(max_y)
+  end
+end
+function star:draw()
+  circfill(self.x, self.y, self.size, self.colour)
+end
+
+-- game
+function start()
+  started = true
+  grav = 0.25
+  del(platforms, start_platform)
+
+  -- create enemies
+  for i = 0,8 do
+    e = enemy:new()
+    e:init()
+    add(enemies, e)
+  end
+end
+function lose()
+  ended = true 
 end
 
 function _init()
   -- disable btnp repeats
   poke(0X5F5C, 255)
 
-  for i = 0,5 do
-    e = enemy:new({
-        x = rnd(127),
-        y = rnd(127),
-        dx = rnd(1),
-        dy = rnd(1)
+  -- create stars
+  for i = 0,35 do
+    s = star:new({
+        x = rnd(max_x),
+        y = rnd(max_y),
+        size = rnd(1.5)
       })
-    add(enemies, e)
+    add(stars, s)
   end
 end
 
 function _update()
-  if (btnp(4)) then p:flap()  end
-  if (btnp(5)) then p:flap()  end
-  if (btn(0)) then p:move_left() end
-  if (btn(1)) then p:move_right() end
-  p:update()
-  for e in all(enemies) do
-    e:update()
+  if not started then
+    if (btnp(4)) then start() end
+    if (btnp(5)) then start() end
+  elseif not ended then
+    i = time()
+    score += flr(i)
+    if (btnp(4)) then p:flap()  end
+    if (btnp(5)) then p:flap()  end
+    if (btn(0)) then p:move_left() end
+    if (btn(1)) then p:move_right() end
+
+    for s in all(stars) do
+      s:update()
+    end
+    for e in all(enemies) do
+      e:update()
+    end
+    p:update()
   end
 end
 
 function _draw()
-  cls(5)
-  p:draw()
+  cls(0)
+  for s in all(stars) do
+    s:draw()
+  end
   for e in all(enemies) do
     e:draw()
+  end
+  for pl in all(platforms) do
+    pl:draw()
+  end
+  p:draw()
+
+  -- lives
+  print("lives: "..p.lives, 8, 8, 7)
+
+  -- score
+  print("score: "..score, 8, 16, 7)
+
+  -- water
+  rectfill(0, max_y, max_x, max_y - 10, 12)
+
+  if ended then
+    print("you lose!", max_x / 2 - 15, max_y / 2, 7)
   end
 end
