756876c0abbc2e129584b92477350ca8199ae071
Jesse Ross <jesseross92@gmail.com>
2024-05-03 15:57:29 -0400

    Added sprites for enemies

--- enemy.lua
+++ enemy.lua
@@ -1,11 +1,17 @@
 
 -- enemy
 enemy = {
-  size = 2,
+  width = sprite_width,
+  height = sprite_height,
+  size = 2, -- remove
   x = 0,
   y = 0,
   dx = 0,
   dy = 0,
+  sprite = 4,
+  sprites = {4, 5, 6, 7, 8},
+  -- coroutines
+  animate = nil
 }
 enemy.__index = enemy
 
@@ -21,10 +27,14 @@ function enemy:update()
   if(self.x < min_x - self.size) then 
     self:init()
   end
+
+  if self.animate and costatus(self.animate) != 'dead' then
+    coresume(self.animate)
+  end
 end
 
 function enemy:draw()
-  circfill(self.x, self.y, self.size, 14)
+  spr(self.sprite, self.x, self.y)
 end
 
 function enemy:new(o)
@@ -36,4 +46,13 @@ function enemy:init()
   self.y = rnd(max_y)
   self.dx = -rnd(2)
   self.dy = rnd(1) * rnd({-1, 1})
+
+  self.animate = cocreate(function()
+    local i = 0
+    while true do
+      i += 1
+      self.sprite = self.sprites[i % count(self.sprites) + 1]
+      yield()
+    end
+  end)
 end
